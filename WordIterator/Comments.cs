46bfc2dd49b5be28849fe018a9e8e0fbd9823d7f
DuncanRitchie <duncanritchie@btinternet.com>
2019-11-18 11:06:08 +0000

    Brought in some Comments code I need to adapt

--- WordIterator/Comments.cs
+++ WordIterator/Comments.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace WordIterator
 {
@@ -10,7 +12,51 @@ namespace WordIterator
     {
         public static void AddComment()
         {
+            //Initialize an instance of Document class
 
+            Document doc = new Document();
+
+            //Load a Word document
+
+            doc.LoadFromFile("template.docx");
+
+            //Get the paragraph that you want to add comment
+
+            Paragraph para = doc.Sections[0].Paragraphs[6];
+
+
+            //Insert a start comment mark at the beginning of the paragraph
+
+            CommentMark startCommentMark = new CommentMark(doc);
+
+            startCommentMark.Type = CommentMarkType.CommentStart;
+
+            para.ChildObjects.Insert(0, startCommentMark);
+
+
+            //Add an end comment mark at the end of the paragraph
+
+            CommentMark endCommentMark = new CommentMark(doc);
+
+            endCommentMark.Type = CommentMarkType.CommentEnd;
+
+            para.ChildObjects.Add(endCommentMark);
+
+
+            //Add a comment to the paragraph and specify the content and author
+
+            Comment comment = new Comment(doc);
+
+            comment.Body.AddParagraph().AppendText("This paragraph explains how to write an abstract.");
+
+            comment.Format.Author = "John";
+
+            para.ChildObjects.Add(comment);
+
+
+            //Save to file
+
+            doc.SaveToFile("CommentOnPara.docx", FileFormat.Docx2013);
         }
     }
 }
