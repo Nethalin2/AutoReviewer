71f6fb0adae90038a7514bf5636926cf352ea766
DuncanRitchie <duncanritchie@btinternet.com>
2019-11-19 13:43:40 +0000

    Wrote ConsoleC funcs to make colourful Console.Writes easier

---
+++ WordIterator/ConsoleC.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordIterator
+{
+    class ConsoleC
+    {
+        public static void Write(string text, ConsoleColor foreground)
+        {
+            Console.ForegroundColor = foreground;
+            Console.Write(text);
+        }
+        public static void Write(string text, ConsoleColor foreground, ConsoleColor background)
+        {
+            Console.ForegroundColor = foreground;
+            Console.BackgroundColor = background;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+        public static void WriteLine(string text, ConsoleColor foreground)
+        {
+            Console.ForegroundColor = foreground;
+            Console.WriteLine(text);
+        }
+        public static void WriteLine(string text, ConsoleColor foreground, ConsoleColor background)
+        {
+            Console.ForegroundColor = foreground;
+            Console.BackgroundColor = background;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+    }
+}
