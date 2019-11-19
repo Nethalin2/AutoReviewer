7c36f95b366c451a6dcdfb87bebd3cf7c443b52e
DuncanRitchie <duncanritchie@btinternet.com>
2019-11-19 15:34:53 +0000

    Program now runs everything useful we have so far

--- WordIterator/Program.cs
+++ WordIterator/Program.cs
@@ -19,10 +19,10 @@ namespace WordIterator
 
             // Comments.AddToEveryPara(doc);
 
-            LanguageChecker();
+            Headers.DetectHeaders(doc);
+            Headers.DetectLineSpacingAfterBullets(doc);
 
-            //Headers.DetectHeaders(doc);
-            //Headers.DetectLineSpacingAfterBullets(doc);
+            LanguageChecker(doc);
 
             //// Save to a new file.
             doc.SaveAs2(Filepath.Full().Replace(".docx", "_2.docx"));
@@ -216,6 +216,90 @@ namespace WordIterator
                 word.Quit();
             }
 
+        }
+
+
+
+        public static void LanguageChecker(Document doc) {
+            try
+            {
+                int count = doc.Words.Count;
+
+                int countUKEnglish = 0;
+                int countUSEnglish = 0;
+                int countNotUKUSEnglish = 0;
+
+                ConsoleC.WriteLine(ConsoleColor.White, "Checking the language of every word...");
+
+                for (int k = 1; k <= count; k++)
+                {
+                    //// Write a marker of where we are in the document every kth word.
+                    if (k % 50 == 0)
+                    {
+                        ConsoleC.Write(ConsoleColor.Black, ConsoleColor.Gray, " " + k + " / " + count + " ");
+                    }
+
+                    string text = doc.Words[k].Text;
+
+                    //// Check language
+
+                    if (doc.Words[k].LanguageID == WdLanguageID.wdEnglishUK)
+                    {
+                        ConsoleC.Write(ConsoleColor.Green, text);
+                        countUKEnglish++;
+                        // ConsoleC.WriteLine(ConsoleColor.Green, "\nThis is a UK/US English word.");
+                    }
+                    else if (doc.Words[k].LanguageID == WdLanguageID.wdEnglishUS)
+                    {
+                        ConsoleC.Write(ConsoleColor.Yellow, text);
+                        countUSEnglish++;
+                        if (countUSEnglish % 10 == 1)
+                        {
+                            Comments.Add(doc, k, "This is US English but should be UK English.");
+                        }
+                    }
+                    else
+                    {
+                        ConsoleC.WriteLine(ConsoleColor.Red, "\n" + text);
+                        ConsoleC.WriteLine(ConsoleColor.Red, "This is not a UK or US English word.");
+                        countNotUKUSEnglish++;
+                        if (countNotUKUSEnglish % 10 == 1)
+                        {
+                            Comments.Add(doc, k, "This is not UK English but should be.");
+                        }
+                    }
+
+
+                    //////// Save to a new file.
+                    ////try
+                    ////{
+                    ////    doc.SaveAs2(Filepath.Full().Replace(".docx", "_2.docx"));
+                    ////}
+                    ////catch (Exception ex)
+                    ////{
+                    ////    ConsoleC.WriteLine(ConsoleColor.Red, "Failed to save new file — " + ex.ToString());
+                    ////}
+
+                }
+
+                //// Give feedback after all the words have been checked.
+                ConsoleC.WriteLine(ConsoleColor.White, "\nFinished checking language.");
+
+                ConsoleC.WriteLine(ConsoleColor.Green, countUKEnglish + " words were UK English. This is good!");
+                if (countUSEnglish > 0)
+                {
+                    ConsoleC.WriteLine(ConsoleColor.Yellow, countUSEnglish + " words were US English. Please change these to UK English.");
+                }
+                if (countNotUKUSEnglish > 0)
+                {
+                    ConsoleC.WriteLine(ConsoleColor.Red, countNotUKUSEnglish + " words were neither. Please change these to UK English.");
+                }
+            }
+            catch (Exception ex)
+            {
+                ConsoleC.WriteLine(ConsoleColor.Red, ex.ToString());
+
+            }
         }
     }
    
