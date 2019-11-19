6a99f039140ea7167d483ff3812c31f66f8dc45f
DuncanRitchie <duncanritchie@btinternet.com>
2019-11-19 16:53:33 +0000

    Wrote InlineLists.IsMatch()

---
+++ WordIterator/InlineLists.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace WordIterator
+{
+    class InlineLists
+    {
+        public static bool IsMatch(String textToCheck)
+        {
+            string[] patterns = {
+                @".*\bi\..*\bii\..*", // Matches i. ii.
+                @".*\ba\..*\bb\..*", // Matches a. b.
+                @".*\b1\..*\b2\..*", // Matches 1. 2.
+                @".*\bi\).*\bii\).*", // Matches i) ii)
+                @".*\ba\).*\bb\).*", // Matches a) b)
+                @".*\b1\).*\b2\).*", // Matches 1) 2)
+                @".*\(i\).*\(ii\).*", // Matches (i) (ii)
+                @".*\(a\).*\(b\).*", // Matches (a) (b)
+                @".*\(1\).*\(2\).*" // Matches (1) (2)
+            };
+            foreach(string pattern in patterns)
+            {
+                if (Regex.IsMatch(textToCheck, pattern, RegexOptions.IgnoreCase))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
